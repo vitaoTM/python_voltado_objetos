51edaf0b4dfd5473ab7d58c4f0e45b06c48b1472
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-17 01:19:26 -0300

    update

--- python_modulo_2/modelo.py
+++ python_modulo_2/modelo.py
@@ -3,7 +3,7 @@ from pyparsing import anyOpenTag
 
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
         self.likes = 0
@@ -16,10 +16,17 @@ class Series:
         self.nome = nome
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 vingadores = Filme('vingadores - Guerra infinita' , 2018, 160)
-print(vingadores.nome)
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
+      f' - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 westworld = Series('westworld', 2016, 4)
 print(f'Nome {westworld.nome} - Ano {westworld.ano} -'
-        f' Temporada {westworld.temporadas}')
+        f' Temporada {westworld.temporadas} -' 
+        f'Likes: {westworld.likes}')
+
