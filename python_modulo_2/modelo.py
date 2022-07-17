b6eeeae0fdc0ca01a4508858e1af006efa613f7a
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-17 15:34:56 -0300

    colocando metodos init e str

--- python_modulo_2/modelo.py
+++ python_modulo_2/modelo.py
@@ -1,13 +1,15 @@
+from re import I
+from numpy import atleast_1d
 from pyparsing import anyOpenTag
-
-
-
 
 class Programa:
     def __init__(self, nome, ano):
         self.__nome = nome.title()
         self.__likes = 0
         self.ano = ano
+    
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
         
     @property
     def likes(self):
@@ -28,19 +30,31 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'  
+
 class Series(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 
 vingadores = Filme('vingadores - Guerra infinita' , 2018, 160)
-vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f' - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
-
 westworld = Series('westworld', 2016, 4)
-print(f'Nome {westworld.nome} - Ano {westworld.ano} -'
-        f' Temporada {westworld.temporadas} -' 
-        f'Likes: {westworld.likes}')
 
+playlist = [vingadores, westworld]
+vingadores.dar_like()
+westworld.dar_like()
+
+for programa in playlist:
+    print(programa)
+
+# print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
+#       f' - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+# print(f'Nome {westworld.nome} - Ano {westworld.ano} -'
+#         f' Temporada {westworld.temporadas} -' 
+#         f'Likes: {westworld.likes}')
