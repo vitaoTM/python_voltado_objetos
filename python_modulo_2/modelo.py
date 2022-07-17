53f29fc81da0b8daa056caff0f5322b0bcbf927f
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-17 13:35:59 -0300

    adicionando classes

--- python_modulo_2/modelo.py
+++ python_modulo_2/modelo.py
@@ -4,24 +4,51 @@ from pyparsing import anyOpenTag
 class Filme:
     def __init__(self, nome, ano, duracao):
         self.__nome = nome.title()
+        self.__likes = 0
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
 
+    @property
+    def likes(self):
+        return self.__likes
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 class Series:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title() 
+        self.__likes = 0       
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
 
     def dar_like(self):
         self.likes += 1
 
+    @property
+    def likes(self):
+        return self.__likes
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
 vingadores = Filme('vingadores - Guerra infinita' , 2018, 160)
+vingadores.dar_like()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
       f' - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
