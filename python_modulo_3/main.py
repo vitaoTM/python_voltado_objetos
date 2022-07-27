ba8b7e843420f1d8590ebc2df7f5aa79ff8316ee
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-26 22:00:20 -0300

    utilizando metodos e laços para procurar uma sting

--- python_modulo_3/main.py
+++ python_modulo_3/main.py
@@ -12,9 +12,12 @@ interrogacao = url.find('?')
 url_parametros = url[interrogacao:]
 # print(url_parametros)
 
-parametro_de_busca = "moedaOrigem"
+parametro_de_busca = "quantidade"
 indice_parametros = url_parametros.find(parametro_de_busca)
 indice_valor = indice_parametros + len(parametro_de_busca) +1
-valor = url_parametros[indice_valor:]
-
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 print(valor)
