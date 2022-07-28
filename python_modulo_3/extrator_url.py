f86c21a392dbd7ed3aa1fb3b71e7cf9e7d87c8c4
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-27 21:51:48 -0300

    Ajeitando codigo e parametros

--- python_modulo_3/extrator_url.py
+++ python_modulo_3/extrator_url.py
@@ -8,3 +8,22 @@ class ExtratorURL:
     def valida_url(self):
         if self.url == "":
             raise ValueError("A URL esta vazia")
+    
+    def get_url_base(self):
+        indice_interrogacao = self.url.find("?")
+        url_base = self.url[:indice_interrogacao]
+        return url_base
+
+    def get_url_parametros(self):
+        indice_interrogacao = self.url.find("?")
+        url_parametros = self.url[indice_interrogacao +1:]
+        return url_parametros
+
+    def get_valor_parametro(self, nome_parametro):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
+        indice_valor = indice_parametro + len(parametro_busca) + 1
+        indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
+        if indice_e_comercial == -1:
+            valor = self.get_url_parametros()[indice_valor:]
+        else:
+            valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
