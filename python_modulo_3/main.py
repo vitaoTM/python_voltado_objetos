1505b0f7890af9bc59da3a6480b908ac94c9c4d5
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-27 21:34:59 -0300

    utilizando metodos replace e strip

--- python_modulo_3/main.py
+++ python_modulo_3/main.py
@@ -3,6 +3,10 @@ from pytz import timezone
 import pytz
 
 url = "bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = url.replace(" ", "")
+
+if url == "":
+    raise ValueError("A url esta vazia")
 
 url_base = url[0:19]
 
