a7fa5714d0fc1faea0420d3bf58e54294684bd97
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-29 15:15:40 -0300

    aprendendo sobre __eq__

--- python_modulo_4/introducao_a_collections.py
+++ python_modulo_4/introducao_a_collections.py
@@ -32,6 +32,24 @@ class ContaCorrente:
         for conta in contas:
             conta.deposita(100)
 
+class ContaSalario:
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+    
+    def __eq__(self, outro):
+        if type(outro) != ContaSalario:
+            return False
+        return self._codigo == outro._codigo
+        
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return f"[>> Codigo {self._codigo} Saldo {self._saldo} <<]"
+
+
 
 conta_do_vi = ContaCorrente(42)
 
