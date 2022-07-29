9f176a89c2cd25feaa69cf28e5bf070dcdf8940e
vitaoTM <vitor.grosskopf@gmail.com>
2022-07-28 22:17:26 -0300

    começando novo modulo

--- python_modulo_4/introducao_a_collections.py
+++ python_modulo_4/introducao_a_collections.py
@@ -1,13 +1,26 @@
 from contextlib import ContextDecorator
+from msilib.schema import Class
 from tkinter.filedialog import SaveFileDialog
+import array as arr
+import numpy as np
 
 
 idades = [39, 30, 27, 18]
+class Conta:
+    def __init__(self, codigo ):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return f"[>> Codigo {self._codigo} Saldo {self._saldo} <<]"
 
 class ContaCorrente:
     def __init__(self, codigo):
         self.codigo = codigo
-        self. saldo = 0
+        self.saldo = 0
 
     def deposita(self, valor):
         self.saldo += valor
@@ -15,12 +28,15 @@ class ContaCorrente:
     def __str__(self):
         return f"[>>Código {self.codigo} Saldo {self.saldo} <<]"
 
+    def deposita_para_contas(contas):
+        for conta in contas:
+            conta.deposita(100)
+
+
 conta_do_vi = ContaCorrente(42)
-print(conta_do_vi)
 
 conta_da_je = ContaCorrente(4750)
 conta_da_je.deposita(1000)
-print(conta_da_je)
 
 contas = [conta_do_vi, conta_da_je]
 
