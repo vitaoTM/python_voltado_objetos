526d02f7cba3e24185efce8f4a73f1b59fb27458
vitaoTM <vitor.grosskopf@gmail.com>
2022-08-02 19:42:56 -0300

    validação de classes

--- python_modulo_5/cpf_cnpj.py
+++ python_modulo_5/cpf_cnpj.py
@@ -1,20 +1,30 @@
 # from validate_docbr import CPF, CNPJ
-from validate_docbr import CPF
+from validate_docbr import CPF, CNPJ
 
-class Cpf:
-    def __init__(self, documento):
+class Cpf_Cnpj:
+    def __init__(self, documento, tipo_documento):
+        self.tipo_documento = tipo_documento
         documento = str(documento)
-        if self.valida_cpf(documento):
-            self.cpf = documento
+        if tipo_documento == 'cpf':
+            if self.valida_cpf(documento):
+                self.cpf = documento
+            else:
+                raise ValueError("CPF invalido")
+        elif self.tipo_documento == 'cnpj':
+            if self.valida_cnpj(documento):
+                self.cnpj = documento
+            else: 
+                raise ValueError("CNPJ Invalido!!")
         else:
-            raise ValueError("CPF invalido")
+             raise ValueError("Documento invalido")
+
 
     def valida_cpf(self, cpf):
         if len(cpf) == 11:
-            validador = CPF()
-            return validador.validate(cpf)
+            validador_cpf = CPF()
+            return validador_cpf.validate(cpf)
         else:
-            return ValueError("Quantidade de Digitos invalida")
+            raise ValueError("Quantidade de Digitos invalida")
 
     def format_Cpf(self):
         mascara = CPF()
@@ -22,3 +32,10 @@ class Cpf:
 
     def __str__(self):
         return self.format_Cpf()
+
+    def valida_cnpj(self, cnpj):
+        if len(cnpj) == 14:
+            validador_cnpj = CNPJ()
+            return validador_cnpj.validate(cnpj)
+        else: 
+            raise ValueError("Quantidade de digitos invalido")
--- python_modulo_5/main.py
+++ python_modulo_5/main.py
@@ -1,10 +1,16 @@
 # from validate_docbr import CPF
 
-from cpf_cnpj import Cpf
+from cpf_cnpj import Cpf_Cnpj
 
-cpf_um = Cpf("04165893919")
+cpf_um = Cpf_Cnpj("04165893919", 'cpf')
 print(cpf_um)
 # obj_cpf = Cpf(cpf)
+
+cnpj_um = "35379838000112"
+
+documento = Cpf_Cnpj(cnpj_um, 'cnpj')
+
+print(documento)
 
 
 
