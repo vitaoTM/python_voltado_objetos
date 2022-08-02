a0cd9785668fe322a343a6cf5e5abd423ea0fea6
vitaoTM <vitor.grosskopf@gmail.com>
2022-08-02 19:49:08 -0300

    testando classes e validapções

--- python_modulo_5/cpf_cnpj.py
+++ python_modulo_5/cpf_cnpj.py
@@ -27,11 +27,19 @@ class Cpf_Cnpj:
             raise ValueError("Quantidade de Digitos invalida")
 
     def format_Cpf(self):
-        mascara = CPF()
-        return mascara.mask(self.cpf)        
+        mask_cpf = CPF()
+        return mask_cpf.mask(self.cpf)
+
+    def format_Cnpj(self):
+        mask_cnpj = CNPJ()
+        return mask_cnpj.mask(self.cnpj)
 
     def __str__(self):
-        return self.format_Cpf()
+        if self.tipo_documento == 'cpf':
+            return self.format_Cpf()
+        elif self.tipo_documento == 'cnpj':
+            return self.format_Cnpj()
+        
 
     def valida_cnpj(self, cnpj):
         if len(cnpj) == 14:
